e881edc7fd02d2bd82fadf286e4f03d70a7729a7
Mais Mofed Alnawa <maisalna733@gmail.com>
2024-10-29 02:35:13 +0300

    modified

--- app_one/models/crm_lead.py
+++ app_one/models/crm_lead.py
@@ -1,6 +1,9 @@
 from odoo import models, fields
 
 
+
+
+
 class Crm(models.Model):
     _inherit = 'crm.lead'
     _description = 'model for crm'
@@ -9,5 +12,4 @@ class Crm(models.Model):
     is_customer = fields.Boolean()
     partner_id = fields.Many2one('res.partner',domain=[('state_patient','=', True)])
     name = fields.Char(default='property')
-
 
